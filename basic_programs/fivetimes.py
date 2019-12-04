89d7b91ee7105f0c4db8565a90e5cb274027f897
jcli1023 <jcli@ryerson.ca>
2019-12-04 06:10:35 -0500

    Basic programs up to writing own functions

---
+++ basic_programs/fivetimes.py
@@ -0,0 +1,10 @@
+print('My name is')
+#for i in range(5):
+#for i in range(12, 16):
+for i in range(0, 10, 2):
+    print('Jimmy Five Times ' + str(i))
+
+i = 0
+while i < 5:
+    print('Jimmy Five Times ' + str(i))
+    i += 1
